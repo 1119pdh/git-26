5931f66429fbb83679bd7f0e0a3d6c103b32d408
1119 pdh <1119pdh@gmail.com>
2021-10-17 21:19:22 +0900

    Third

---
+++ test/test12.py
@@ -0,0 +1,13 @@
+def is_okay_to_drive(who):
+    if who == "son":
+        return"Nope!";
+    elif who == "dad":
+        return"Good!";
+    elif who == "grand father":
+        return"Be careful!";
+    else:
+        return"Who are you?";
+    
+  
+
+print(is_okay_to_drive("son"))
