dbd0990a51c2c00de7838b980bd2157a5aa01192
1119 pdh <1119pdh@gmail.com>
2021-10-20 20:03:16 +0900

    secondVersion

--- movie/moviemovie/moviestar/models.py
+++ movie/moviemovie/moviestar/models.py
@@ -1,3 +1,27 @@
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
+from django.db.models.deletion import CASCADE
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    date_of_birth = models.DateField(default=timezone.now)
+
+    class Meta:
+        db_table = 'actors'
+
+class Movie(models.Model):
+    title = models.CharField(max_length=45)
+    release_date = models.DateField(default=timezone.now)
+    running_time = models.IntegerField()
+
+    class Meta:
+        db_table = 'movies'
+
+class Actor_movie(models.Model):
+    actor = models.ForeignKey('actor', on_delete=models.CASCADE)
+    movie = models.ForeignKey('movie', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'actor_movies'
--- movie/moviemovie/moviestar/views.py
+++ movie/moviemovie/moviestar/views.py
@@ -1,3 +1,93 @@
+import json
+
+from django.db.models.fields import EmailField
+from django.http import HttpResponse, JsonResponse, request
+from django.views import View
 from django.shortcuts import render
 
+from .models import Actor, Actor_movie
+from moviestar.models import Movie
+
 # Create your views here.
+
+class ActorListView(View):
+    def post(self, request):
+        data= json.loads(request.body)
+
+        Actor.objects.create(
+            first_name = data["first_name"],
+            last_name = data["last_name"],
+            date_of_birth = data["date_of_birth"]
+        )
+        return JsonResponse({"message" : "SUCCESS"}, status= 201)
+
+    def get(self, request):
+        results = []
+
+        actors = Actor.objects.all()
+        for actor in actors:
+            actors_movies = Actor_movie.objects.filter(actor_id=actor.id)
+            movie_list= []
+            for actor_movie in actors_movies:
+                  movie_list.append(
+                       {
+                       "id"    : actor_movie.movie.id, 
+                       "title" : actor_movie.movie.title
+                       }
+                  )
+            results.append(
+                  {
+                      "first_name" : actor.first_name, 
+                      "last_name" : actor.last_name,
+                      "movie" : movie_list         
+                  }
+            )
+        return JsonResponse({"actors" : results}, status= 200)          
+
+
+
+class MovieListView(View):
+    def post(self,request):
+        data= json.loads(request.body)
+        
+        Movie.objects.create(
+            title = data["title"],
+            release_date = data["release_date"],
+            running_time = data["running_time"]
+        )
+
+        return JsonResponse({"message" : "SUCCESS"}, status= 201)
+
+    def get(self,request):
+        results=[]
+
+        movies = Movie.objects.all()
+        
+        actor_list=[]
+        for movie in movies:
+            actors = movie.actor_movie.actor_set.all()
+            actor_list=[]
+            for actor in actors:
+                actor_list.append(
+                    {
+                    "name" : actor.first_name + actor.last_name
+                    }   
+                )
+        results.append(
+            {
+                "title" : movie.title ,
+                "release_date" : movie.releas_date
+            }
+        )
+        return JsonResponse({"movies" : results}, status= 200)
+
+class Actor_MoviesListView(View):
+    def post(self,request):
+        data= json.loads(request.body)
+
+        Actor_movie.objects.create(
+            actor= Actor.objects.get(last_name = data["actor_id"]),
+            movie= Movie.objects.get(title = data["movie_id"])
+        )
+
+        return JsonResponse({"message" : "SUCCESS"}, status= 201)
